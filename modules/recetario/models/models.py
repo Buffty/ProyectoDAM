caab40a28299d1ea37a66b3b451e3cf16bf92875
salva alcover fuster <salvakanals@gmail.com>
2020-05-05 16:41:09 +0200

    .

--- modules/recetario/models/models.py
+++ modules/recetario/models/models.py
@@ -1,24 +1,15 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, tools
+from openerp.exceptions import ValidationError
+from datetime import datetime, timedelta
 
 class recetas(models.Model):
      _name = 'recetario.recetas'
-
      nombre = fields.Char()
      descripcion = fields.Char()
      tipo = fields.Char()
      dificultad = fields.Char()
      duracion = fields.Char()
      usuario_id = fields.Many2one('res.partner')
-
-
-class usuario(models.Model):
-    _inherit = 'res.partner'
-    _name = 'res.partner'
-    passwd = fields.Char()
-
-    id_recetario = fields.One2many('recetario.recetas','usuario_id',ondelete='cascade')
-    aplicacion = fields.Boolean()
-
 
