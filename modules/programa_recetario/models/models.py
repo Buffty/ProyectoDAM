636d94496d7f2018e20e43fab38e69d3af9e8f13
salva alcover fuster <salvakanals@gmail.com>
2020-05-05 17:07:46 +0200

    .

--- modules/programa_recetario/models/models.py
+++ modules/programa_recetario/models/models.py
@@ -5,9 +5,3 @@ from odoo import models, fields, api
 class recetas(models.Model):
     _name = 'programa_recetario.recetas'
     name = fields.Char()
-
-class usuarios(models.Model):
-    _inherit = 'res.partner'
-    _name = 'res.partner'
-    passwd = fields.Char()
-    is_app = fields.Boolean()
