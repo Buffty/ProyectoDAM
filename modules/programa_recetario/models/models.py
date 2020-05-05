109713155c93489ec619d13cda4b2cea2b1332f1
salva alcover fuster <salvakanals@gmail.com>
2020-05-05 17:36:37 +0200

    Perfi

--- modules/programa_recetario/models/models.py
+++ modules/programa_recetario/models/models.py
@@ -7,9 +7,15 @@ from datetime import datetime, timedelta
 class recetas(models.Model):
     _name = 'programa_recetario.recetas'
     name = fields.Char()
+    descripcion = fields.Char()
+    tipo = fields.Char()
+    dificultad = fields.Char()
+    duracion = fields.Char()
+    usuarios_id = fields.Many2one('res.partner')
 
 class usuarios(models.Model):
     _inherit = 'res.partner'
     _name = 'res.partner'
     passwd = fields.Char()
     is_app = fields.Boolean()
+    recetas_id = fields.One2many('programa_recetario.recetas', 'usuarios_id', ondelete='cascade')
