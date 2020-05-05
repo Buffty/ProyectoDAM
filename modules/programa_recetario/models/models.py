4b24e8a4c70e6e6e06c26530beadd820326f3fa2
salva alcover fuster <salvakanals@gmail.com>
2020-05-05 16:52:51 +0200

    .

--- modules/programa_recetario/models/models.py
+++ modules/programa_recetario/models/models.py
@@ -2,14 +2,6 @@
 
 from odoo import models, fields, api
 
-# class programa_recetario(models.Model):
-#     _name = 'programa_recetario.programa_recetario'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+class recetas(models.Model):
+    _name = 'programa_recetario.recetas'
+    name = fields.Char()
