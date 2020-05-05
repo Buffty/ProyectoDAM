5e6262da4edd6bbeaa062a8d85413bdcfa35558f
salva alcover fuster <salvakanals@gmail.com>
2020-05-05 17:25:06 +0200

    .

--- modules/programa_recetario/__manifest__.py
+++ modules/programa_recetario/__manifest__.py
@@ -10,17 +10,17 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Mi compañia indentificada",
+    'website': "http://www.Noexiste.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Categoría',
+    'version': '0.2',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','mail','sale'],
 
     # always loaded
     'data': [
--- modules/programa_recetario/models/models.py
+++ modules/programa_recetario/models/models.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, tools
+from openerp.exceptions import ValidationError
+from datetime import datetime, timedelta
 
 class recetas(models.Model):
     _name = 'programa_recetario.recetas'
