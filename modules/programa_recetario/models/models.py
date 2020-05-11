0cda15d3e291b2bb7b038f002e9969336a3bb5db
salva alcover fuster <salvakanals@gmail.com>
2020-05-11 15:50:15 +0200

    .

--- modules/programa_recetario/models/models.py
+++ modules/programa_recetario/models/models.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api, tools
-from openerp.exceptions import ValidationError
-from datetime import datetime, timedelta
 
 class recetas(models.Model):
     _name = 'programa_recetario.recetas'
